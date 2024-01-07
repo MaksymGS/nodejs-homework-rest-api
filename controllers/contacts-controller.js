5fa209963fde92d2f6ed8b61e1371ed78d569e81
MaksymGS <130307586+MaksymGS@users.noreply.github.com>
2024-01-07 23:16:10 +0200

    04 Branch Initial commit

---
+++ controllers/contacts-controller.js
@@ -0,0 +1,58 @@
+import Contact from "../models/Contact.js";
+import { HttpError } from "../helpers/index.js";
+import { ctrlWrapper } from "../decorators/index.js";
+
+const getContacts = async (req, res) => {
+  const result = await Contact.find({}, "-createdAt -updatedAt");
+  res.json(result);
+};
+
+const getContactById = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findById(id, "-createdAt -updatedAt");
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json(result);
+};
+
+const addContact = async (req, res) => {
+  const result = await Contact.create(req.body);
+  res.status(201).json(result);
+};
+
+const updateContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body);
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json(result);
+};
+
+const updateStatusContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndUpdate(id, req.body);
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json(result);
+};
+
+const removeContact = async (req, res) => {
+  const { id } = req.params;
+  const result = await Contact.findByIdAndDelete(id);
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json({ message: "contact deleted" });
+};
+
+export default {
+  getContacts: ctrlWrapper(getContacts),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  updateContact: ctrlWrapper(updateContact),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
+  removeContact: ctrlWrapper(removeContact),
+};
---
+++ routes/api/contacts-router.js
@@ -0,0 +1,41 @@
+import express from "express";
+import contactsController from "../../controllers/contacts-controller.js";
+import { validateBody, isEmptyBody } from "../../decorators/index.js";
+import {
+  contactAddSchema,
+  contactUpdateSchema,
+  contactUpdateFavoriteSchema,
+} from "../../models/Contact.js";
+import {isValidId } from "../../middlewares/index.js";
+
+const contactsRouter = express.Router();
+
+contactsRouter.get("/", contactsController.getContacts);
+
+contactsRouter.get("/:id", isValidId, contactsController.getContactById);
+
+contactsRouter.post(
+  "/",
+  isEmptyBody("body must have fields"),
+  validateBody(contactAddSchema),
+  contactsController.addContact
+);
+
+contactsRouter.delete("/:id", isValidId, contactsController.removeContact);
+
+contactsRouter.put(
+  "/:id",
+  isValidId,
+  validateBody(contactUpdateSchema),
+  isEmptyBody("body must have fields"),
+  contactsController.updateContact
+);
+
+contactsRouter.patch(
+  "/:id/favorite",
+  isValidId,
+  isEmptyBody("missing field favorite"),
+  validateBody(contactUpdateFavoriteSchema),
+  contactsController.updateStatusContact
+);
+export default contactsRouter;
