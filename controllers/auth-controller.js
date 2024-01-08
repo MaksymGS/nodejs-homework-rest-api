ca8910433d895e1cc40afcf7d66d87875fd0c6ac
MaksymGS <maximgsua@gmail.com>
2024-01-08 16:51:10 +0200

    add jwt

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -2,6 +2,9 @@ import User from "../models/User.js";
 import { HttpError } from "../helpers/index.js";
 import { ctrlWrapper } from "../decorators/index.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const { JWT_SECRET } = process.env;
 
 const signup = async (req, res) => {
   const { email, password } = req.body;
@@ -21,14 +24,23 @@ const signup = async (req, res) => {
 const signin = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
+
   if (!user) {
     throw HttpError(401, "Email or password is wrong");
   }
+
   const passwordCompare = await bcrypt.compare(password, user.password);
+
   if (!passwordCompare) {
     throw HttpError(401, "Email or password is wrong");
   }
-  const token = "123.5555.555";
+
+  const { _id: id } = user;
+  const payload = {
+    id,
+  };
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "23h" });
+
   res.json({
     token,
     user: { email: user.email, subscription: user.subscription },
