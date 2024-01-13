7b1e1b87619081d2c09cae0e3540bb09c676923b
MaksymGS <130307586+MaksymGS@users.noreply.github.com>
2024-01-13 23:49:00 +0200

    fix updateC, updateStatus

--- controllers/contacts-controller.js
+++ controllers/contacts-controller.js
@@ -23,7 +23,7 @@ const addContact = async (req, res) => {
 
 const updateContact = async (req, res) => {
   const { id } = req.params;
-  const result = await Contact.findByIdAndUpdate(id, req.body);
+  const result = await Contact.findByIdAndUpdate(id, req.body, {new: true});
   if (!result) {
     throw HttpError(404);
   }
@@ -32,7 +32,7 @@ const updateContact = async (req, res) => {
 
 const updateStatusContact = async (req, res) => {
   const { id } = req.params;
-  const result = await Contact.findByIdAndUpdate(id, req.body);
+  const result = await Contact.findByIdAndUpdate(id, req.body, {new: true});
   if (!result) {
     throw HttpError(404);
   }
