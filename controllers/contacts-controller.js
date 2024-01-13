77c2dabff9b1e8b825f5f523d24a767bffc7d97c
MaksymGS <130307586+MaksymGS@users.noreply.github.com>
2024-01-14 00:35:40 +0200

    05 Branch Initial commit

---
+++ controllers/contacts-controller.js
@@ -0,0 +1,73 @@
+import Contact from "../models/Contact.js";
+import { HttpError } from "../helpers/index.js";
+import { ctrlWrapper } from "../decorators/index.js";
+
+const getContacts = async (req, res) => {
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 20, favorite } = req.query;
+  const skip = (page - 1) * limit;
+  const searchParams = { owner };
+  if (favorite) {
+    searchParams.favorite = favorite;
+  }
+  const result = await Contact.find(searchParams, "-createdAt -updatedAt", {
+    skip,
+    limit,
+  }).populate("owner", "email");
+  res.json(result);
+};
+
+const getContactById = async (req, res) => {
+  const { id: _id } = req.params;
+  const { _id: owner } = req.user;
+  const result = await Contact.findOne({ _id, owner }, "-createdAt -updatedAt");
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json(result);
+};
+
+const addContact = async (req, res) => {
+  const { _id: owner } = req.user;
+  const result = await Contact.create({ ...req.body, owner });
+  res.status(201).json(result);
+};
+
+const updateContact = async (req, res) => {
+  const { id: _id } = req.params;
+  const { _id: owner } = req.user;
+  const result = await Contact.findOneAndUpdate({ _id, owner }, req.body);
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json(result);
+};
+
+const updateStatusContact = async (req, res) => {
+  const { id: _id } = req.params;
+  const { _id: owner } = req.user;
+  const result = await Contact.findOneAndUpdate({ _id, owner }, req.body);
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json(result);
+};
+
+const removeContact = async (req, res) => {
+  const { id: _id } = req.params;
+  const { _id: owner } = req.user;
+  const result = await Contact.findOneAndDelete({ _id, owner });
+  if (!result) {
+    throw HttpError(404);
+  }
+  res.json({ message: "contact deleted" });
+};
+
+export default {
+  getContacts: ctrlWrapper(getContacts),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  updateContact: ctrlWrapper(updateContact),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
+  removeContact: ctrlWrapper(removeContact),
+};
